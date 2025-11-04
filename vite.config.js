ede22597a6e1524b2b620c45576ce1578860ae4c
Forma y Fondo <126127852+SRDis@users.noreply.github.com>
2025-11-04 12:48:39 -0600

    Set base path for production in Vite config
    Add base path for production assets and routes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 🔹 Esto asegura que los assets y rutas internas funcionen en producción
 })
